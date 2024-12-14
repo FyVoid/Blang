e381a3892382d801504f8681697ff53b2a013b96
FyVoid <mythstudiofyvoid@outlook.com>
2024-12-14 12:21:31 +0800

    add main page

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.cpp
+ * @author fyvoid (fyvo1d@outlook.com)
+ * @brief main function of blang
+ * @version 0.1
+ * @date 2024-12-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include "blang.hpp"
 
 using blang::Blang;
